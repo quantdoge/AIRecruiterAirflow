4877ab5650f666ee07295a6465ef4db1b00feb12
quantdoge <ce9d919@protonmail.com>
2025-06-16 20:37:17 +0800

    Added timeout

--- dags/test_jd_etl.py
+++ dags/test_jd_etl.py
@@ -20,23 +20,27 @@ with DAG(
     task1 = BashOperator(
         task_id='2_uat_continuous',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 2_uat_continuous.py',  # Example: fails
+        execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
     )
 
     task2 = BashOperator(
         task_id='3_uat_webingest_first',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 3_uat_webingest_first.py',  # Example: succeeds
+        execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task1's result
     )
 
     task3 = BashOperator(
         task_id='4_uat_index_summ',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 4_uat_index_summ.py',
+        execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task2's result
     )
 
     task4 = BashOperator(
         task_id='5_uat_upsert_md',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 5_uat_upsert_md.py',
+        execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task2's result
     )
 
