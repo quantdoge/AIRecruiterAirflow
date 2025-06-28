a3a6714413168425d68e1c31e65f3fb020a16bd7
quantdoge <ce9d919@protonmail.com>
2025-06-28 19:53:39 +0800

    etl- remove step 2 and increase frequency

--- dags/test_jd_etl.py
+++ dags/test_jd_etl.py
@@ -13,27 +13,21 @@ default_args = {
 with DAG(
     dag_id='test_jd_etl',
     default_args=default_args,
-    schedule_interval='0 */3 * * *',  # Every 3 hour
-    tags=['AIRecruiter', 'ETL', '2_uat_continuous','3_uat_webingest_first','4_uat_index_summ', '5_uat_upsert_md'],
+    schedule_interval='0 */6 * * *',  # Every 6 hour
+    tags=['AIRecruiter', 'ETL', '3_uat_webingest_first','4_uat_index_summ', '5_uat_upsert_md'],
 ) as dag:
-
-    task1 = BashOperator(
-        task_id='2_uat_continuous',
-        bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 2_uat_continuous.py',  # Example: fails
-        execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
-    )
 
     task2 = BashOperator(
         task_id='3_uat_webingest_first',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 3_uat_webingest_first.py',  # Example: succeeds
-        execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
+        execution_timeout=timedelta(seconds=7200),  # Times out after 20 minutes
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task1's result
     )
 
     task3 = BashOperator(
         task_id='4_uat_index_summ',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 4_uat_index_summ.py',
-        execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
+        execution_timeout=timedelta(seconds=3600),  # Times out after 20 minutes
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task2's result
     )
 
@@ -44,4 +38,4 @@ with DAG(
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task2's result
     )
 
-    task1 >> task2 >> task3 >> task4
+    task2 >> task3 >> task4
