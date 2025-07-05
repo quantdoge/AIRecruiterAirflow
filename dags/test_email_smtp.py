12967f5948a7f59211f4212d7e0111fe7fb1b988
quantdoge <ce9d919@protonmail.com>
2025-07-05 10:55:58 +0800

    email test job added

---
+++ dags/test_email_smtp.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.email import EmailOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="test_email_smtp",
+    start_date=datetime(2025, 7, 5),
+    schedule_interval=None,  # Run manually
+    catchup=False,
+) as dag:
+
+    send_test_email = EmailOperator(
+        task_id="send_test_email",
+        to="limjs4spotify@gmail.com",  # Replace with your email
+        subject="Airflow SMTP Test",
+        html_content="<h3>This is a test email from Airflow SMTP configuration.</h3>",
+    )
