a0ab75687b4d54ab5ce05e16f6e87c01790d22f7
quantdoge <ce9d919@protonmail.com>
2025-07-07 21:21:55 +0800

    amended test_jd_etl

--- dags/test_jd_etl.py
+++ dags/test_jd_etl.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2025, 5, 24),
+    'start_date': datetime(2025, 7, 6),
     'catchup': False,
     'email': ["limjs4spotify@gmail.com"],
     'email_on_failure': True
@@ -30,22 +30,22 @@ with DAG(
     task2 = BashOperator(
         task_id='3_uat_webingest_first',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 3_uat_webingest_first.py',  # Example: succeeds
-        #execution_timeout=timedelta(seconds=7200),  # Times out after 20 minutes
+        execution_timeout=timedelta(seconds=7200),  # Times out after 20 minutes
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task1's result
     )
 
     task3 = BashOperator(
         task_id='4_uat_index_summ',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 4_uat_index_summ.py',
-        #execution_timeout=timedelta(seconds=3600),  # Times out after 20 minutes
+        execution_timeout=timedelta(seconds=3600),  # Times out after 20 minutes
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task2's result
     )
 
     task4 = BashOperator(
         task_id='5_uat_upsert_md',
         bash_command='source /home/airflow/airflow-project/airflow-env/bin/activate && cd /home/airflow/airflow-project/AIRecruiter/ETL/Debug && python 5_uat_upsert_md.py',
-        #execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
+        execution_timeout=timedelta(seconds=1200),  # Times out after 20 minutes
         trigger_rule=TriggerRule.ALL_DONE,     # Run regardless of task2's result
     )
 
-    [task2, task3, task4]
+    task2>> task3>> task4
